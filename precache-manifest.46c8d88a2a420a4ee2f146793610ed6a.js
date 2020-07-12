0a0ef5c04cc01a461f7eed4c25453c9783fd5f86
Hammadsoloman <hammadali95@yahoo.com>
2020-07-13 02:38:20 +0300

    Updates

---
+++ precache-manifest.46c8d88a2a420a4ee2f146793610ed6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6fb8ede15d292a237bf155d117da67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242dc67333647efc6d07",
+    "url": "/static/css/main.ed01d13f.chunk.css"
+  },
+  {
+    "revision": "fe361a5d3a7bc2771681",
+    "url": "/static/js/2.ceb8685b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ceb8685b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242dc67333647efc6d07",
+    "url": "/static/js/main.cf930057.chunk.js"
+  },
+  {
+    "revision": "ed194ace71b62e98c3a8",
+    "url": "/static/js/runtime-main.656650cb.js"
+  }
+]);
